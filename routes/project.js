7fbfa49de15c478d05fd4e12f4ca86a257f8a4c6
Zachary Wu <zawu@ucsd.edu>
2014-02-20 10:13:03 +0000

    finishing up lab7

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,12 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
+  console.log(projectID);
   // query for the specific project and
   // call the following callback
+  models.Project
+    .find({"_id": projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,6 +21,20 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project({
+    "title": form_data.title,
+    "date": form_data.date,
+    "summary": form_data.summary,
+    "image": form_data.image
+  });
+  //form_data.save(afterSaving);
+  newProject.save(afterSaving);
+
+  function afterSaving(err) {
+    if(err) {console.log(err); res.send(500);}
+    res.redirect('/');
+  }
+
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +42,15 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(afterRemoving);
+
+    function afterRemoving(err) {
+      if(err)
+        console.log(err);
+        res.send();
+    }
+
 }
